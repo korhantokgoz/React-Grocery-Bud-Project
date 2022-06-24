c2b6eddf4eafe0209096e22862cc64aa30b35524
Sam Smith <korhan.tokgoz92@gmail.com>
2022-06-24 14:10:20 +0300

    Updates

---
+++ precache-manifest.8efa9887c930dba39fb81612100576f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f857c9c3042bca68e8fd27af275d656b",
+    "url": "/React-Grocery-Bud-Project/index.html"
+  },
+  {
+    "revision": "59408f5fd12e986c511d",
+    "url": "/React-Grocery-Bud-Project/static/css/main.a903c182.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/React-Grocery-Bud-Project/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Grocery-Bud-Project/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59408f5fd12e986c511d",
+    "url": "/React-Grocery-Bud-Project/static/js/main.f4f38afe.chunk.js"
+  },
+  {
+    "revision": "af58dbc21b989e183b53",
+    "url": "/React-Grocery-Bud-Project/static/js/runtime-main.3b1094e8.js"
+  }
+]);
